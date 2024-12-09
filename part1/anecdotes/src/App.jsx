e19f7e352433eeab64eeb9c28e5961ed5fd8da0f
mikoplxs <kjscpf@gmail.com>
2024-12-09 15:27:29 +0100

    1.13, 1.14 do dokonczenia

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -12,21 +12,43 @@ const App = () => {
     'The only way to go fast, is to go well.'
   ]
 
-  const anecdotes_votes = [0, 0, 0, 0, 0, 0, 0, 0]
-   
+
   const [selected, setSelected] = useState(0)
+  const [anecdotes_votes, setValue] = useState([1,5,3,2,1,6,4,2])
+  const [anecdote_highest_text, setText] = useState(anecdotes[selected])
+  const [anecdote_highest_vote, setVote] = useState(0)
+
+
+  const voteClick = () => {
+    const votes_copy = {...anecdotes_votes}
+    votes_copy[selected] += 1
+    setValue(votes_copy)
+
+    setVote(Math.max(...anecdotes_votes))
+
+
+  }
+  
 
   return (
     <div>
+      <h2>Anecdote of the day</h2>
       {anecdotes[selected]}
       <div>
-        {anecdotes_votes[selected]}
+        has {anecdotes_votes[selected]} votes
       </div>
       <div>
-        <button onClick={() => setSelected(Math.floor(Math.random() * (anecdotes.length + 1)))}>
+        <button onClick={() => setSelected(Math.floor(Math.random() * ((anecdotes.length - 1) + 1)))}>
           next anecdote
         </button>
+        <button onClick={() => voteClick()}>
+          vote
+        </button>
+
       </div>
+      <h2>Anecdote with most votes</h2>
+      {anecdote_highest_text}
+      {anecdote_highest_vote}
     </div>
   )
 }
