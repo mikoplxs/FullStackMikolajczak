427a6c87781d67d9f017d8c2a42b058ba753ec25
mikoplxs <kjscpf@gmail.com>
2025-02-21 12:21:16 +0100

    2.5

---
+++ part2/courseinfo/src/components/Course.jsx
@@ -0,0 +1,54 @@
+const Part = (props) => {
+    return (
+      <div>
+        <p>N: {props.part} <br></br>E: {props.ex}</p>
+      </div>
+    )
+  }
+  
+  
+const Content = (props) => {
+  
+    return (
+      <div>
+        {props.course.map(part => 
+          <Part key={part.id} part={part.name} ex={part.exercises}/>
+        )}
+      </div>
+    )
+  }
+
+const Header = (props) => {
+    return (
+      <div>
+        <h1>{props.course}</h1>
+      </div>
+    )
+  }
+  
+const Total = (props) => {
+  
+    const total = props.course.reduce((acc,item) => acc+item.exercises, 0,)
+  
+    return (
+      <div>
+        Total: {total}
+      </div>
+  
+    )
+  
+  }
+
+const Course = (props) => {
+    return (
+      <div>
+          {props.seal.map(seal => [
+            <Header course={seal.name}/>,
+            <Content course={seal.parts}/>,
+            <Total course={seal.parts}/>]
+          )}
+      </div>
+    )
+  }
+
+export default Course
